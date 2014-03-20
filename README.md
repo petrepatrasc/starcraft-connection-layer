35f87b135a740852c424e78bc0f990ee179a7150
Petre Patrasc <petre@dreamlabs.ro>
2014-03-20 02:05:26 +0200

    Initial commit of this bundle.

---
+++ README.md
@@ -0,0 +1,4 @@
+Starcraft Connection Layer
+==========================
+
+Currently keeping this a secret as it is very early in development, so hush hush.
---
+++ StarcraftConnectionLayerBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace petrepatrasc\StarcraftConnectionLayerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class StarcraftConnectionLayerBundle extends Bundle
+{
+}
